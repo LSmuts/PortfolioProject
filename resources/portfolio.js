bfbb1c90d753b43093b3ba2323540e32c580b063
LSmuts <132968213+LSmuts@users.noreply.github.com>
2023-07-24 15:30:08 +1000

    Change navbar layout

--- resources/portfolio.js
+++ resources/portfolio.js
@@ -1,1 +1,12 @@
 // JavaScript Document
+
+// JavaScript to toggle the dropdown menu
+document.addEventListener("DOMContentLoaded", function () {
+  var hamburgerMenu = document.querySelector(".hamburger-menu");
+  var navbarRightHidden = document.querySelector(".navbar-right-hidden");
+
+  hamburgerMenu.addEventListener("click", function () {
+  console.log("Hamburger menu clicked");
+  navbarRightHidden.classList.toggle("show-dropdown");
+  });
+});
